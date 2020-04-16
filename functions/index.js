7c500f6a55fe065ab32fc89eac414b1f3bcd404a
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-04-16 13:33:27 +0200

    Create post

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,22 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 admin.initializeApp();
+
+const express = require('express');
+const app = express();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello World!");
+  response.send('Hello World!');
 });
 
 exports.getPosts = functions.https.onRequest((req, res) => {
   admin
     .firestore()
-    .collection("posts")
+    .collection('posts')
     .get()
     .then((data) => {
       let posts = [];
@@ -24,3 +27,25 @@ exports.getPosts = functions.https.onRequest((req, res) => {
     })
     .catch((err) => console.error(err));
 });
+
+exports.createPost = functions.https.onRequest((req, res) => {
+    if(req.method !== 'POST'){return res.status(400).json({error: 'Method not allowed'})}
+  const newPost = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  admin
+    .firestore()
+    .collection('posts')
+    .add(newPost)
+    .then((doc) => {
+        res.json({ message: `document ${doc.id} created successfully` });
+      })
+    .catch((err) => {
+      res.status(500).json({ error: 'Something went wron'g });
+      console.error(err);
+    });
+});
+
