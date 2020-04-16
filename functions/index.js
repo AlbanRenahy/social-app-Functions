2dd3b1c954c25a44a6d53c642fd3720930a09ebb
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-04-16 14:47:58 +0200

    Signup route

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,24 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const app = require ('express')();
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+
+var config = {
+  apiKey: "AIzaSyC4wVCgle4ykHKhYQCB9im-S6KsCRSycm8",
+  authDomain: "social-app-c4995.firebaseapp.com",
+  databaseURL: "https://social-app-c4995.firebaseio.com",
+  projectId: "social-app-c4995",
+  storageBucket: "social-app-c4995.appspot.com",
+  messagingSenderId: "56575194694",
+  appId: "1:56575194694:web:93afa85816c29a9f58fc47",
+  measurementId: "G-4YNGPF9D87"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get('/posts', (req, res) => {
   admin
@@ -57,6 +71,28 @@ app.post('/post', (req, res) => {
       console.error(err);
     });
 });
+
+// Signup route
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+  
+  // TODO validate data
+  
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+  .then(data => {
+    return res.status(201).json({message: `user ${data.user.uid} signed up successfully`})
+  })
+  .catch(err => {
+    console.err(err);
+    return res.status(500).json({ error: err.code })
+  });
+});
+
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
 
