2827bc129588a3a8dd3a2d0a5069f0a0b131cbbc
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-04-17 11:12:54 +0200

    implementing authentification middleware with Token JWT

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ admin.initializeApp({
 });
 
 var config = {
-  apiKey: 'AIzaSyC4wVCgle4ykHKhYQCB9im-S6KsCRSycm8',
+  apiKey: "AIzaSyC4wVCgle4ykHKhYQCB9im-S6KsCRSycm8",
   authDomain: process.env.AUTH_DOMAIN,
   databaseURL: process.env.DATABASE_URL,
   projectId: process.env.PROJECT_ID,
@@ -48,15 +48,49 @@ app.get("/posts", (req, res) => {
     });
 });
 
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer ')
+  ) {
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.error('No token found');
+    return res.status(403).json({ error: 'Unauthorized' });
+  }
+
+   admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch((err) => {
+      console.error('Error while verifying token ', err);
+      return res.status(403).json(err);
+    });
+};
+
 // Create one post
-app.post("/post", (req, res) => {
+app.post("/post", FBAuth, (req, res) => {
   if (req.body.body.trim() === "") {
     return res.status(400).json({ body: "Body must not be empty" });
   }
 
   const newPost = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString(),
   };
 
