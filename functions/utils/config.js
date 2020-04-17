2300bc9a239008fccb0a99e487bf918f5fd01136
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-04-17 14:22:13 +0200

    Upload images route

--- functions/utils/config.js
+++ functions/utils/config.js
@@ -1,11 +1,11 @@
 
 module.exports = {
     apiKey: "AIzaSyC4wVCgle4ykHKhYQCB9im-S6KsCRSycm8",
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
+    authDomain: "social-app-c4995.firebaseapp.com",
+    databaseURL: "https://social-app-c4995.firebaseio.com/",
+    projectId:"social-app-c4995",
+    storageBucket: "social-app-c4995.appspot.com",
+    messagingSenderId: "56575194694",
+    appId: "1:56575194694:web:93afa85816c29a9f58fc47",
     // measurementId: MEASUREMENT_ID,
   };
